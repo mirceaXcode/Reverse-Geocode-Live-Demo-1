f5dcfd7188d93a891683b57b321a6f747edf59cf
Mircea Popescu <mirceap@gmail.com>
2018-09-04 15:50:10 -0400

    Initial Commit

---
+++ Reverse Geocode Live Demo 1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reverse Geocode Live Demo 1
+//
+//  Created by Mircea Popescu on 9/4/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Reverse Geocode Live Demo 1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Reverse Geocode Live Demo 1
+//
+//  Created by Mircea Popescu on 9/4/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

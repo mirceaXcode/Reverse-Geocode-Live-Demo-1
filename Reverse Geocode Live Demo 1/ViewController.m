a2d848168aaa822a55798008584e0107d4b332c6
Mircea Popescu <mirceap@gmail.com>
2018-09-04 17:25:13 -0400

    !st Working version using a Push button to do the reverse GeoLocation

--- Reverse Geocode Live Demo 1/ViewController.m
+++ Reverse Geocode Live Demo 1/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "MapKit/MapKit.h"
+#import "CoreLocation/CoreLocation.h"
+
 
 @interface ViewController ()
+
+@property (strong, nonatomic) CLGeocoder *geocoder;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UILabel *reverseGeocodeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *pinIcon;
 
 @end
 
@@ -16,14 +24,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.geocoder = [[CLGeocoder alloc] init];
+    self.reverseGeocodeLabel.text = nil;
+    self.reverseGeocodeLabel.alpha = 0.5;
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)reverseGeocodeTapped:(id)sender {
+    CLLocationCoordinate2D coord = [self locationAtCenterOfMapView];
+    CLLocation *loc = [[CLLocation alloc] initWithLatitude:coord.latitude longitude:coord.longitude];
+    [self startReverseGeocodeLocation:loc];
+    
 }
 
+#pragma mark - Private
+
+-(CLLocationCoordinate2D) locationAtCenterOfMapView {
+    // Calculate center of the space in the ImageView coord space
+    CGPoint centerOfPin = CGPointMake(CGRectGetMidX(self.pinIcon.bounds), CGRectGetMidY(self.pinIcon.bounds));
+    // Convert it into our mapView coord space
+    return [self.mapView convertPoint:centerOfPin toCoordinateFromView:self.pinIcon];
+}
+
+-(void) startReverseGeocodeLocation: (CLLocation *) location {
+    
+    [_geocoder reverseGeocodeLocation:location completionHandler:
+     ^(NSArray *placemarks, NSError *error){
+         if (error){
+             UIAlertController *ac = [UIAlertController alertControllerWithTitle:@"There was a problem reverse geocoding" message:[error localizedDescription] preferredStyle:UIAlertControllerStyleAlert];
+             [self presentViewController:ac animated:YES completion:nil];
+             return;
+         }
+         // Grab some interesting bits of CLPlaceMark and show it
+         // But no duplicates!
+         NSMutableSet *mappedPlaceDescs = [NSMutableSet new];
+         for (CLPlacemark *p in placemarks){
+             if (p.name != nil)
+                 [mappedPlaceDescs addObject:p.name];
+             if (p.administrativeArea != nil)
+                 [mappedPlaceDescs addObject:p.administrativeArea];
+             if (p.country != nil)
+                 [mappedPlaceDescs addObject:p.country];
+             [mappedPlaceDescs addObjectsFromArray:p.areasOfInterest];
+         }
+         self.reverseGeocodeLabel.text =[[ mappedPlaceDescs allObjects] componentsJoinedByString:@"\n"];
+         self.reverseGeocodeLabel.alpha = 1.0;
+     }];
+}
 
 @end
